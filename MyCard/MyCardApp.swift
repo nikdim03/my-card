d818be1f3487643b14407b44f481288ccd8da769
Dmitriy Nikulin <nikdim_03@mail.ru>
2022-03-27 16:10:01 +0300

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Dmitriy on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
